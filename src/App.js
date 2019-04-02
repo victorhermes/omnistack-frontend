c3cde09f3f9026a548757973b51da47690d49f81
Victor Hermes <me.victorhermes@gmail.com>
2019-04-02 19:37:30 -0300

    styled-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Provider } from "react-redux";
 import Routes from "./routes";
 import store from "./store";
 
+import GlobalStyle from "./styles/global";
+
 const App = () => (
     <Provider store={store}>
-        <Routes />
+        <Fragment>
+            <GlobalStyle />
+            <Routes />
+        </Fragment>
     </Provider>
 );
 
