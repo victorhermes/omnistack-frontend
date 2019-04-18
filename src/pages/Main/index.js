8aaa4c27d3fc70caf8b3638ab64e48739059337a
Victor Hermes <me.victorhermes@gmail.com>
2019-04-17 21:09:58 -0300

    Main loading

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 import TeamSwitcher from "~/components/TeamSwitcher";
 import { Container } from "./styles";
-
+import Gif from "~/elips.svg";
 class Main extends Component {
-    componentDidMount() {}
+    state = {
+        isLoading: true
+    };
+
+    componentDidMount() {
+        this.setState({ isLoading: false });
+    }
 
     render() {
+        const { isLoading } = this.state;
+
+        if (isLoading) {
+            return "CARREGANDO";
+        }
         return (
             <Container>
                 <TeamSwitcher />
